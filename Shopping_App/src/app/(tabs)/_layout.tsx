715369d567aa7364f296420cb1c1c69a4aea223d
Sameera Kuna <sameera@gsstech.in>
2026-05-28 09:11:32 +0530

    sign in fixed

--- Shopping_App/src/app/(tabs)/_layout.tsx
+++ Shopping_App/src/app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
+import { useCallback } from 'react';
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
-import { View, Text, StyleSheet, useCallback } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { CoveColors } from '@/constants/theme';
 import { useWishlist } from '@/context';
 import { useAppSelector } from '@/redux/hooks';
